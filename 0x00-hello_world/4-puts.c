fd58f97b8bc71923aac84c9570b45fee7b3f1b70
LinusWanyoike <linus10.linus@gmail.com>
2023-02-16 07:00:49 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - Aprogram that prints in a new line usint the puts 
  * Return: 0 (success)
- */
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
