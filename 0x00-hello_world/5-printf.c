12c6b85ddf435617029f19b85a6b4344f7e1ba36
LinusWanyoike <linus10.linus@gmail.com>
2023-02-16 09:41:09 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
  * main - A program to print out
- * return 0 (success)
+ * Return: 0 on success
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
