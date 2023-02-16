88e6826aadd59d5d7c7005ae4f16ff3c2d71f0fa
LinusWanyoike <linus10.linus@gmail.com>
2023-02-16 09:03:52 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - write a programe that states: with proper grammer
+ * return (0) (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome
+is a piece of art,\n");
+return (0);
+}
